def3530b9dcc3212caef015469e5f8dc66664608
Rob Brennan <rob@therobbrennan.com>
2024-03-14 17:26:35 -0700

    FUTURE: Default to port 3000 after attempting to use the PORT environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = 3000;
+const port = 3000; // FUTURE: Default to port 3000 after attempting to use the PORT environment variable
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
