dc058211ea6c25a91f7a700fa234421c414d4fbc
Rob Brennan <rob@therobbrennan.com>
2024-03-15 16:42:05 -0700

    Remove default port. Use environment variable only.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import app from './app';
 
-// Use the PORT environment variable, default to 3000 if not set
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
